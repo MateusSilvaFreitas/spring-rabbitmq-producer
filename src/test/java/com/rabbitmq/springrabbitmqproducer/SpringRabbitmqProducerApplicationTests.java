226f2234ab8914e4af6649363e520b486600caa1
Mateus Silva Freitas <mateus.freitas@paytrack.com.br>
2022-02-21 20:34:44 -0300

    commit

---
+++ src/test/java/com/rabbitmq/springrabbitmqproducer/SpringRabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.springrabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
